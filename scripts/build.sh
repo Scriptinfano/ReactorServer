d9a55160eb202b9464df7162b50ce447aa31d8f7
Maverick <scripterMonster@protonmail.com>
2024-11-14 20:36:15 +0800

    增加线程池

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,15 @@
 # Created Time: 三 10/30 19:58:22 2024
 #########################################################################
 #!/bin/bash
+# 获取当前路径的最后一个文件夹名称
+current_dir_name=$(basename "$PWD")
+
+# 检查是否为ReactorServer
+if [ "$current_dir_name" != "ReactorServer" ]; then
+  echo "请在项目根目录下运行此脚本文件"
+  exit 1
+fi
+
 cd build
 cmake ..
 make
-chmod +x ../bin/main
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string.h>
+#include<vector>
 using namespace std;
 
 int main(int argc, char **argv)
@@ -7,6 +8,7 @@ int main(int argc, char **argv)
     char tmp[1024] = {0};
     const char *str = "123";
     memcpy(tmp, str, strlen(str));
-
+    vector<string> strings;
+    strings=
     cout << tmp << endl;
 }
